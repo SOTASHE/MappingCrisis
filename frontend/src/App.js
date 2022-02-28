9ab802548b80e15c2556996124787bc66f6793d9
Mountain-Sky <owens@superiorbizsolutions.com>
2022-02-28 20:09:47 +0200

    Register,Login,reportCrisis

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,10 +9,30 @@ import axios from "axios";
 
 import { format } from "timeago.js";
 
+import Register from "./components/Register";
+import Login from "./components/Login";
+
 export default function App() {
   const [crisis, setCrisis] = useState([]);
 
+  //const currentUser = "ANDI";
+
+  // const myStorage = window.localStorage;
+  // const [currentUsername, setCurrentUsername] = useState(
+  //  myStorage.getItem("user")
+  // / );
+
+  const myStorage = window.localStorage;
+  const [currentUsername, setCurrentUsername] = useState(
+    myStorage.getItem("user")
+  );
+
   const [currentPlaceId, setCurrentPlaceId] = useState(null);
+
+  const [newPlace, setNewPlace] = useState(null);
+
+  const [tag, setTag] = useState(null);
+  const [desc, setDesc] = useState(null);
 
   const [viewport, setViewport] = useState({
     width: "100vw",
@@ -24,6 +44,9 @@ export default function App() {
   });
 
   const [showPopup, setShowPopup] = React.useState(true);
+
+  const [showRegister, setShowRegister] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
 
   useEffect(() => {
     const getCrisis = async () => {
@@ -43,28 +66,69 @@ export default function App() {
     setViewport({ ...viewport, latitude: lat, longitude: long });
   };
 
+  const handleAddClick = (e) => {
+    const [longitude, latitude] = e.lngLat;
+    setNewPlace({
+      lat: latitude,
+      long: longitude,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newCrisis = {
+      username: currentUsername,
+      tag,
+      desc,
+      lat: newPlace.lat,
+      long: newPlace.long,
+    };
+
+    try {
+      const res = await axios.post("/crisis", newCrisis);
+      setCrisis([...crisis, res.data]);
+      setNewPlace(null);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleLogout = () => {
+    setCurrentUsername(null);
+    myStorage.removeItem("user");
+  };
   return (
     <>
       <div>
-        <h2>Durban!</h2>
         <ReactMapGL
           {...viewport}
           onViewportChange={setViewport}
           mapboxApiAccessToken="pk.eyJ1Ijoic25heWUtc290YXNoZSIsImEiOiJjbDA2MHN4dTEwYzR5M2pubmk0Mmo2aDk3In0.LxXpxLF9MffWbjrL-_ZJLA"
           mapStyle="mapbox://styles/mapbox/streets-v11"
           mapStyle="mapbox://styles/snaye-sotashe/cl06q57at000315p3hc5r7m6o"
+          onDblClick={handleAddClick}
+          transitionDuration="200"
         >
           {crisis.map((crisis) => (
             <>
               <Marker
                 latitude={crisis.lat}
                 longitude={crisis.long}
-                offsetLeft={-20}
-                offsetTop={-10}
+                offsetLeft={-viewport.zoom * 3.5}
+                offsetTop={-viewport.zoom * 7}
               >
                 <Room
-                  style={{ fontSize: viewport.zoom * 7, color: "slateblue" }}
-                  onClick={() => handleMarkerClick(crisis._id)}
+                  style={{
+                    fontSize: viewport.zoom * 7,
+                    color:
+                      crisis.username === currentUsername
+                        ? "tomato"
+                        : "slateblue",
+                    cursor: "pointer",
+                  }}
+                  onClick={() =>
+                    handleMarkerClick(crisis._id, crisis.lat, crisis.long)
+                  }
                 />
               </Marker>
 
@@ -82,7 +146,7 @@ export default function App() {
                   latitude={crisis.lat}
                   longitude={crisis.long}
                   anchor="left"
-                  onClose={() => setShowPopup(false)}
+                  onClose={() => setCurrentPlaceId(null)}
                 >
                   <div className="card">
                     <label> {crisis.tag} </label>
@@ -101,6 +165,65 @@ export default function App() {
               )}
             </>
           ))}
+          {newPlace && (
+            <Popup
+              latitude={newPlace.lat}
+              longitude={newPlace.long}
+              closeButton={true}
+              closeOnClick={false}
+              onClose={() => setNewPlace(null)}
+              anchor="left"
+            >
+              <div>
+                <form onSubmit={handleSubmit}>
+                  <label>tag</label>
+                  <input
+                    placeholder="Enter a crisis in area"
+                    autoFocus
+                    onChange={(e) => setTag(e.target.value)}
+                  />
+                  <label>Description</label>
+                  <textarea
+                    placeholder="Say more about this crisis."
+                    onChange={(e) => setDesc(e.target.value)}
+                  />
+
+                  <button type="submit" className="submitButton">
+                    Report Crisis
+                  </button>
+                </form>
+              </div>
+            </Popup>
+          )}
+
+          {currentUsername ? (
+            <button className="button logout" onClick={handleLogout}>
+              Log out
+            </button>
+          ) : (
+            <div className="buttons">
+              <button
+                className="button login"
+                onClick={() => setShowLogin(true)}
+              >
+                Log in
+              </button>
+              <button
+                className="button register"
+                onClick={() => setShowRegister(true)}
+              >
+                Register
+              </button>
+            </div>
+          )}
+          {showRegister && <Register setShowRegister={setShowRegister} />}
+          {showLogin && (
+            <Login
+              setShowLogin={setShowLogin}
+              setCurrentUsername={setCurrentUsername}
+              myStorage={myStorage}
+            />
+          )}
         </ReactMapGL>
       </div>
     </>
