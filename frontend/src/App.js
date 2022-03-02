34bf79a924f3f65cddfc2f82f150b64b0129de25
Mountain-Sky <owens@superiorbizsolutions.com>
2022-03-02 23:08:37 +0200

    added a loader spin, shows when fetching data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,9 +11,11 @@ import { format } from "timeago.js";
 
 import Register from "./components/Register";
 import Login from "./components/Login";
+import Loader from "./components/Loader";
 
 export default function App() {
   const [crisis, setCrisis] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const myStorage = window.localStorage;
   const [currentUsername, setCurrentUsername] = useState(
@@ -43,12 +45,14 @@ export default function App() {
 
   useEffect(() => {
     const getCrisis = async () => {
+      setLoading(true);
       try {
         const res = await axios.get("/crisis");
         setCrisis(res.data);
       } catch (err) {
         console.log(err);
       }
+      setLoading(false);
     };
 
     getCrisis();
@@ -93,124 +97,128 @@ export default function App() {
   return (
     <>
       <div>
-        <ReactMapGL
-          {...viewport}
-          onViewportChange={setViewport}
-          mapboxApiAccessToken="pk.eyJ1Ijoic25heWUtc290YXNoZSIsImEiOiJjbDA2MHN4dTEwYzR5M2pubmk0Mmo2aDk3In0.LxXpxLF9MffWbjrL-_ZJLA"
-          mapStyle="mapbox://styles/mapbox/streets-v11"
-          mapStyle="mapbox://styles/snaye-sotashe/cl06q57at000315p3hc5r7m6o"
-          onDblClick={handleAddClick}
-          transitionDuration="200"
-        >
-          {crisis.map((crisis) => (
-            <>
-              <Marker
-                latitude={crisis.lat}
-                longitude={crisis.long}
-                offsetLeft={-viewport.zoom * 3.5}
-                offsetTop={-viewport.zoom * 7}
-              >
-                <Room
-                  style={{
-                    fontSize: viewport.zoom * 7,
-                    color:
-                      crisis.username === currentUsername
-                        ? "tomato"
-                        : "slateblue",
-                    cursor: "pointer",
-                  }}
-                  onClick={() =>
-                    handleMarkerClick(crisis._id, crisis.lat, crisis.long)
-                  }
-                />
-              </Marker>
-
-              {crisis._id === currentPlaceId && (
-                <Popup
-                  key={crisis._id}
+        {!loading ? (
+          <ReactMapGL
+            {...viewport}
+            onViewportChange={setViewport}
+            mapboxApiAccessToken="pk.eyJ1Ijoic25heWUtc290YXNoZSIsImEiOiJjbDA2MHN4dTEwYzR5M2pubmk0Mmo2aDk3In0.LxXpxLF9MffWbjrL-_ZJLA"
+            mapStyle="mapbox://styles/mapbox/streets-v11"
+            mapStyle="mapbox://styles/snaye-sotashe/cl06q57at000315p3hc5r7m6o"
+            onDblClick={handleAddClick}
+            transitionDuration="200"
+          >
+            {crisis.map((crisis) => (
+              <>
+                <Marker
                   latitude={crisis.lat}
                   longitude={crisis.long}
-                  closeButton={true}
-                  closeOnClick={false}
-                  onClose={() => setCurrentPlaceId(null)}
-                  anchor="left"
+                  offsetLeft={-viewport.zoom * 3.5}
+                  offsetTop={-viewport.zoom * 7}
                 >
-                  <div className="card">
-                    <label>Crisis Tag</label>
-                    <h4 className="place">{crisis.tag}</h4>
+                  <Room
+                    style={{
+                      fontSize: viewport.zoom * 7,
+                      color:
+                        crisis.username === currentUsername
+                          ? "tomato"
+                          : "slateblue",
+                      cursor: "pointer",
+                    }}
+                    onClick={() =>
+                      handleMarkerClick(crisis._id, crisis.lat, crisis.long)
+                    }
+                  />
+                </Marker>
+
+                {crisis._id === currentPlaceId && (
+                  <Popup
+                    key={crisis._id}
+                    latitude={crisis.lat}
+                    longitude={crisis.long}
+                    closeButton={true}
+                    closeOnClick={false}
+                    onClose={() => setCurrentPlaceId(null)}
+                    anchor="left"
+                  >
+                    <div className="card">
+                      <label>Crisis Tag</label>
+                      <h4 className="place">{crisis.tag}</h4>
+                      <label>Description</label>
+                      <p className="desc">{crisis.desc}</p>
+
+                      <label>Information</label>
+                      <span className="username">
+                        Created by <b>{crisis.username}</b>
+                      </span>
+                      <span className="date">{format(crisis.createdAt)}</span>
+                    </div>
+                  </Popup>
+                )}
+              </>
+            ))}
+            {newPlace && (
+              <Popup
+                latitude={newPlace.lat}
+                longitude={newPlace.long}
+                closeButton={true}
+                closeOnClick={false}
+                onClose={() => setNewPlace(null)}
+                anchor="left"
+              >
+                <div>
+                  <form onSubmit={handleSubmit}>
+                    <label>tag</label>
+                    <input
+                      placeholder="Enter a crisis in area"
+                      autoFocus
+                      onChange={(e) => setTag(e.target.value)}
+                    />
                     <label>Description</label>
-                    <p className="desc">{crisis.desc}</p>
+                    <textarea
+                      placeholder="Say more about this crisis."
+                      onChange={(e) => setDesc(e.target.value)}
+                    />
 
-                    <label>Information</label>
-                    <span className="username">
-                      Created by <b>{crisis.username}</b>
-                    </span>
-                    <span className="date">{format(crisis.createdAt)}</span>
-                  </div>
-                </Popup>
-              )}
-            </>
-          ))}
-          {newPlace && (
-            <Popup
-              latitude={newPlace.lat}
-              longitude={newPlace.long}
-              closeButton={true}
-              closeOnClick={false}
-              onClose={() => setNewPlace(null)}
-              anchor="left"
-            >
-              <div>
-                <form onSubmit={handleSubmit}>
-                  <label>tag</label>
-                  <input
-                    placeholder="Enter a crisis in area"
-                    autoFocus
-                    onChange={(e) => setTag(e.target.value)}
-                  />
-                  <label>Description</label>
-                  <textarea
-                    placeholder="Say more about this crisis."
-                    onChange={(e) => setDesc(e.target.value)}
-                  />
+                    <button type="submit" className="submitButton">
+                      Report Crisis
+                    </button>
+                  </form>
+                </div>
+              </Popup>
+            )}
 
-                  <button type="submit" className="submitButton">
-                    Report Crisis
-                  </button>
-                </form>
+            {currentUsername ? (
+              <button className="button logout" onClick={handleLogout}>
+                Log out
+              </button>
+            ) : (
+              <div className="buttons">
+                <button
+                  className="button login"
+                  onClick={() => setShowLogin(true)}
+                >
+                  Log in
+                </button>
+                <button
+                  className="button register"
+                  onClick={() => setShowRegister(true)}
+                >
+                  Register
+                </button>
               </div>
-            </Popup>
-          )}
-
-          {currentUsername ? (
-            <button className="button logout" onClick={handleLogout}>
-              Log out
-            </button>
-          ) : (
-            <div className="buttons">
-              <button
-                className="button login"
-                onClick={() => setShowLogin(true)}
-              >
-                Log in
-              </button>
-              <button
-                className="button register"
-                onClick={() => setShowRegister(true)}
-              >
-                Register
-              </button>
-            </div>
-          )}
-          {showRegister && <Register setShowRegister={setShowRegister} />}
-          {showLogin && (
-            <Login
-              setShowLogin={setShowLogin}
-              setCurrentUsername={setCurrentUsername}
-              myStorage={myStorage}
-            />
-          )}
-        </ReactMapGL>
+            )}
+            {showRegister && <Register setShowRegister={setShowRegister} />}
+            {showLogin && (
+              <Login
+                setShowLogin={setShowLogin}
+                setCurrentUsername={setCurrentUsername}
+                myStorage={myStorage}
+              />
+            )}
+          </ReactMapGL>
+        ) : (
+          <Loader />
+        )}
       </div>
     </>
   );
